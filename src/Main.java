8f92d18a089aec11b14b3a2d97933564efc4440d
Sozonchik <den.sozon@mail.ru>
2022-10-05 21:11:39 +0500

    Сделала 2.2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,50 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Домашнее задание 1.1
+        System.out.println("Задание 1.1");
+int month = 0;
+int total = 0;
+int salary = 15000;
+        while (total <= 2_459_000) {
+             total = total + total / 100;
+            total+=salary;
+            month++;
+            System.out.println("Месяц " + month + " сумма накоплении равна " + total);
+
+        }
+
+        System.out.println("Задание 1.2");
+        int j = 1;
+        while (j <=10){
+            System.out.print(j++ + " ");
+        }
+        System.out.println();
+        for (int i = 10; i > 0; i--){
+            System.out.print(i + " ");
+        }
+        System.out.println("Задание 1.3");
+        int y = 12_000_000;
+        int numberYear = 0;
+        for (int i = 1; i <=10; i++){
+numberYear = (y / 1000) * (17 - 8);
+y = y + numberYear;
+            System.out.println("Год " + i + " численность населения составляет " + y);
+        }
+
+        System.out.println("Задание 2.1");
+        int total2 = 15000;
+        for (int i = 1; total2 <= 12000000; i++) {
+            total2 = total2 + total2 / 100 * 7;
+            System.out.println("Месяц " + i + " сумма накоплении составляет " + total2);
+        }
+        System.out.println("Задание 2.2");
+        int total3 = 15000;
+        for (int i = 1; total3 <= 12_000_000; i++){
+            total3 = total3 + total3 / 100 * 7;
+            if (i % 6 == 0){
+                System.out.println("Месяц " + i + " сумма накоплении составляет" + total3);
+            }
+        }
+
     }
 }
